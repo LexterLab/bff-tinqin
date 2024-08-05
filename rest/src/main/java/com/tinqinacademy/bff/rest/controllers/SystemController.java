59e70216e943dcf559df1ed8cdd2cc6f386e5c23
moldovexc <alexanderparpulansky@gmail.com>
2024-08-05 14:05:14 +0300

    Controller method for creating room

---
+++ rest/src/main/java/com/tinqinacademy/bff/rest/controllers/SystemController.java
@@ -0,0 +1,42 @@
+package com.tinqinacademy.bff.rest.controllers;
+
+import com.tinqinacademy.bff.api.createroom.CreateRoom;
+import com.tinqinacademy.bff.api.createroom.CreateRoomInput;
+import com.tinqinacademy.bff.api.createroom.CreateRoomOutput;
+import com.tinqinacademy.bff.api.errors.ErrorOutput;
+import com.tinqinacademy.hotel.api.RestAPIRoutes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.vavr.control.Either;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "System REST APIs")
+public class SystemController  extends BaseController {
+
+    private final CreateRoom createRoom;
+
+
+    @Operation(
+            summary = "Create Room Rest API",
+            description = "Create Room Rest API is for searching visitor registrations"
+    )
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "201", description = "HTTP STATUS 201 CREATED"),
+            @ApiResponse(responseCode = "400", description = "HTTP STATUS 400 BAD REQUEST"),
+            @ApiResponse(responseCode = "403", description = "HTTP STATUS 403 FORBIDDEN"),
+    })
+    @PostMapping(RestAPIRoutes.CREATE_ROOM)
+    public ResponseEntity<?> createRoom(@RequestBody CreateRoomInput input) {
+        Either<ErrorOutput, CreateRoomOutput> result = createRoom.process(input);
+        return handleOutput(result, HttpStatus.CREATED);
+    }
+}
