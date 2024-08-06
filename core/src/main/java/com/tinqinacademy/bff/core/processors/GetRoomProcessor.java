2e922e5f781fe9bac95e753157c25a86fd1801de
moldovexc <alexanderparpulansky@gmail.com>
2024-08-06 12:11:38 +0300

    Add get room operation to bff

---
+++ core/src/main/java/com/tinqinacademy/bff/core/processors/GetRoomProcessor.java
@@ -0,0 +1,40 @@
+package com.tinqinacademy.bff.core.processors;
+
+import com.tinqinacademy.hotel.api.errors.ErrorOutput;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoom;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoomInput;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoomOutput;
+import com.tinqinacademy.hotel.restexport.HotelClient;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+import jakarta.validation.Validator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Service;
+
+import static io.vavr.API.Match;
+
+@Service
+@Slf4j
+public class GetRoomProcessor extends BaseProcessor implements GetRoom {
+    private final HotelClient hotelClient;
+    public GetRoomProcessor(ConversionService conversionService, Validator validator, HotelClient hotelClient) {
+        super(conversionService, validator);
+        this.hotelClient = hotelClient;
+    }
+
+    @Override
+    public Either<ErrorOutput, GetRoomOutput> process(GetRoomInput input) {
+        log.info("Start getRoom  {}", input);
+        return Try.of(() -> {
+           GetRoomOutput output = hotelClient.getRoomById(input.getRoomId());
+           log.info("End getRoom  {}", output);
+           return output;
+        }).toEither()
+                .mapLeft(throwable -> Match(throwable).of(
+                        validatorCase(throwable),
+                        feignCase(throwable),
+                        defaultCase(throwable)
+                ));
+    }
+}
--- rest/src/main/java/com/tinqinacademy/bff/rest/controllers/HotelController.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/controllers/HotelController.java
@@ -4,15 +4,22 @@ import com.tinqinacademy.hotel.api.RestAPIRoutes;
 import com.tinqinacademy.hotel.api.enumerations.BathroomType;
 import com.tinqinacademy.hotel.api.enumerations.BedSize;
 import com.tinqinacademy.hotel.api.errors.ErrorOutput;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoom;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoomInput;
+import com.tinqinacademy.hotel.api.operations.getroom.GetRoomOutput;
 import com.tinqinacademy.hotel.api.operations.searchroom.SearchRoom;
 import com.tinqinacademy.hotel.api.operations.searchroom.SearchRoomInput;
 import com.tinqinacademy.hotel.api.operations.searchroom.SearchRoomOutput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +30,7 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class HotelController extends BaseController {
     private final SearchRoom searchRoom;
+    private final GetRoom getRoom;
 
     @GetMapping(RestAPIRoutes.SEARCH_ROOMS)
     public ResponseEntity<?> searchRooms(
@@ -40,6 +48,23 @@ public class HotelController extends BaseController {
                         .startDate(startDate)
                         .bedCount(bedCount)
                         .build());
+        return handleOutput(output, HttpStatus.OK);
+    }
+
+    @Operation(
+            summary = "Get Room By Id Rest API",
+            description = "Get Room By Id REST API is used for retrieving a room by id"
+    )
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "HTTP STATUS 200 SUCCESS"),
+            @ApiResponse(responseCode = "400", description = "HTTP STATUS 400 BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "HTTP STATUS 404 NOT FOUND")
+    }
+    )
+    @GetMapping(RestAPIRoutes.GET_ROOM_DETAILS)
+    public ResponseEntity<?> getRoomById(@PathVariable String roomId) {
+        Either<ErrorOutput, GetRoomOutput> output = getRoom.process(GetRoomInput.builder()
+                .roomId(roomId).build());
         return handleOutput(output, HttpStatus.OK);
     }
 }
