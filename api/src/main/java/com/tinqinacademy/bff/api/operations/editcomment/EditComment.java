99ade2cf96d7efc4a3ce6e89173751778ed2cdf4
moldovexc <alexanderparpulansky@gmail.com>
2024-08-10 19:23:25 +0300

    Add edit comment op to bff

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/editcomment/EditComment.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.bff.api.operations.editcomment;
+
+import com.tinqinacademy.bff.api.base.OperationProcessor;
+
+public interface EditComment extends OperationProcessor<EditCommentRequest, EditCommentResponse> {
+}
