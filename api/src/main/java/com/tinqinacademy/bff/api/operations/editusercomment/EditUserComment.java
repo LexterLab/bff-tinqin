59110c7333f8c3531d19b20316172666ad26f49c
moldovexc <alexanderparpulansky@gmail.com>
2024-08-10 21:28:24 +0300

    Add edit user comment op contracts

---
+++ api/src/main/java/com/tinqinacademy/bff/api/operations/editusercomment/EditUserComment.java
@@ -0,0 +1,6 @@
+package com.tinqinacademy.bff.api.operations.editusercomment;
+
+import com.tinqinacademy.bff.api.base.OperationProcessor;
+
+public interface EditUserComment extends OperationProcessor<EditUserCommentRequest, EditUserCommentResponse> {
+}
