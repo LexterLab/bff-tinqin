55e4d9e3e0fc8287733cb2a646c5a5ae5cf3b4b8
moldovexc <alexanderparpulansky@gmail.com>
2024-08-22 12:16:21 +0300

    IT for book room bff op

--- rest/src/test/java/com/tinqinacademy/bff/rest/controllers/HotelControllerTest.java
+++ rest/src/test/java/com/tinqinacademy/bff/rest/controllers/HotelControllerTest.java
@@ -2,6 +2,14 @@ package com.tinqinacademy.bff.rest.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tinqinacademy.authentication.api.exceptions.ResourceNotFoundException;
+import com.tinqinacademy.authentication.api.operations.generateaccesstoken.GetUsernameFromTokenOutput;
+import com.tinqinacademy.authentication.api.operations.getuser.GetUserOutput;
+import com.tinqinacademy.authentication.api.operations.loaduserdetails.LoadUserDetailsInput;
+import com.tinqinacademy.authentication.api.operations.loaduserdetails.LoadUserDetailsOutput;
+import com.tinqinacademy.authentication.api.operations.validateacesstoken.ValidateAccessTokenInput;
+import com.tinqinacademy.authentication.api.operations.validateacesstoken.ValidateAccessTokenOutput;
+import com.tinqinacademy.authentication.restexport.AuthenticationClient;
+import com.tinqinacademy.bff.api.operations.bookroom.BookRoomRequest;
 import com.tinqinacademy.hotel.api.RestAPIRoutes;
 import com.tinqinacademy.hotel.api.enumerations.BathroomType;
 import com.tinqinacademy.hotel.api.enumerations.BedSize;
@@ -12,10 +20,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,6 +51,9 @@ class HotelControllerTest {
 
     @MockBean
     private HotelClient hotelClient;
+
+    @MockBean
+    private AuthenticationClient authenticationClient;
 
     @Test
     void shouldRespondWithRoomDataAndOkStatus() throws Exception {
@@ -70,13 +88,179 @@ class HotelControllerTest {
     }
 
     @Test
-    void shouldRespondWithNotFoundWhenProvidedUnknownRoomIdWhenGettingRoomDetails() throws Exception {
-        String roomId = UUID.randomUUID().toString();
+    void shouldRespondWithCreatedWhenBookingRoom() throws Exception {
+        String roomId = "923364b0-4ed0-4a7e-8c23-ceb5c238ceee";
 
-        when(hotelClient.getRoomById(roomId)).thenThrow(ResourceNotFoundException.class);
+        BookRoomRequest request = BookRoomRequest
+                .builder()
+                .startDate(LocalDateTime.now().plusMonths(1))
+                .endDate(LocalDateTime.now().plusMonths(1).plusWeeks(1))
+                .firstName("George")
+                .lastName("Russell")
+                .phoneNo("+35983323232434")
+                .build();
 
-        mockMvc.perform(get(RestAPIRoutes.GET_ROOM_DETAILS, roomId))
-                .andExpect(status().isNotFound());
+        String accessToken = "token";
+
+        User userDetails = (User) User.withUsername("domino222")
+                .password("password")
+                .authorities("ROLE_USER")
+                .build();
+
+        GetUserOutput getUserOutput = GetUserOutput
+                .builder()
+                .id(UUID.randomUUID())
+                .build();
+
+        GetUsernameFromTokenOutput getUsernameFromTokenOutput = GetUsernameFromTokenOutput.builder()
+                .username("domino222")
+                .build();
+
+        when(authenticationClient.loadUserDetails(any(LoadUserDetailsInput.class)))
+                .thenReturn(LoadUserDetailsOutput.builder().userDetails(userDetails).build());
+
+        ValidateAccessTokenOutput validateAccessTokenOutput = ValidateAccessTokenOutput.builder()
+                .success(true)
+                .build();
+
+        when(authenticationClient.getUsernameFromToken(any())).thenReturn(getUsernameFromTokenOutput);
+        when(authenticationClient.validateToken(any(ValidateAccessTokenInput.class))).thenReturn(validateAccessTokenOutput);
+        when(authenticationClient.getUser(getUsernameFromTokenOutput.getUsername())).thenReturn(getUserOutput);
+
+        mockMvc.perform(post(RestAPIRoutes.BOOK_ROOM, roomId)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
     }
 
+    @Test
+    void shouldRespondWithBadRequestWhenBookingRoomWithInvalidId() throws Exception {
+        String roomId = "invalid";
+
+        BookRoomRequest request = BookRoomRequest
+                .builder()
+                .startDate(LocalDateTime.now().plusMonths(1))
+                .endDate(LocalDateTime.now().plusMonths(1).plusWeeks(1))
+                .firstName("George")
+                .lastName("Russell")
+                .phoneNo("+35983323232434")
+                .build();
+
+        String accessToken = "token";
+
+        User userDetails = (User) User.withUsername("domino222")
+                .password("password")
+                .authorities("ROLE_USER")
+                .build();
+
+        GetUsernameFromTokenOutput getUsernameFromTokenOutput = GetUsernameFromTokenOutput.builder()
+                .username("domino222")
+                .build();
+
+
+        when(authenticationClient.loadUserDetails(any(LoadUserDetailsInput.class)))
+                .thenReturn(LoadUserDetailsOutput.builder().userDetails(userDetails).build());
+
+        ValidateAccessTokenOutput validateAccessTokenOutput = ValidateAccessTokenOutput.builder()
+                .success(true)
+                .build();
+
+        when(authenticationClient.getUsernameFromToken(any())).thenReturn(getUsernameFromTokenOutput);
+        when(authenticationClient.validateToken(any(ValidateAccessTokenInput.class))).thenReturn(validateAccessTokenOutput);
+
+        mockMvc.perform(post(RestAPIRoutes.BOOK_ROOM, roomId)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldRespondWithUnauthorizedWhenBookingRoomWithInvalidToken() throws Exception {
+        String roomId = "invalid";
+
+        BookRoomRequest request = BookRoomRequest
+                .builder()
+                .startDate(LocalDateTime.now().plusMonths(1))
+                .endDate(LocalDateTime.now().plusMonths(1).plusWeeks(1))
+                .firstName("George")
+                .lastName("Russell")
+                .phoneNo("+35983323232434")
+                .build();
+
+        String accessToken = "token";
+
+        User userDetails = (User) User.withUsername("domino222")
+                .password("password")
+                .authorities("ROLE_USER")
+                .build();
+
+        GetUsernameFromTokenOutput getUsernameFromTokenOutput = GetUsernameFromTokenOutput.builder()
+                .username("domino222")
+                .build();
+
+
+        when(authenticationClient.loadUserDetails(any(LoadUserDetailsInput.class)))
+                .thenReturn(LoadUserDetailsOutput.builder().userDetails(userDetails).build());
+
+        ValidateAccessTokenOutput validateAccessTokenOutput = ValidateAccessTokenOutput.builder()
+                .success(false)
+                .build();
+
+        when(authenticationClient.getUsernameFromToken(any())).thenReturn(getUsernameFromTokenOutput);
+        when(authenticationClient.validateToken(any(ValidateAccessTokenInput.class))).thenReturn(validateAccessTokenOutput);
+
+        mockMvc.perform(post(RestAPIRoutes.BOOK_ROOM, roomId)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void shouldRespondWithForbiddenWhenBookingRoomWithInsufficientRole() throws Exception {
+        String roomId = "invalid";
+
+        BookRoomRequest request = BookRoomRequest
+                .builder()
+                .startDate(LocalDateTime.now().plusMonths(1))
+                .endDate(LocalDateTime.now().plusMonths(1).plusWeeks(1))
+                .firstName("George")
+                .lastName("Russell")
+                .phoneNo("+35983323232434")
+                .build();
+
+        String accessToken = "token";
+
+        User userDetails = (User) User.withUsername("domino222")
+                .password("password")
+                .authorities("ROLE_NONE")
+                .build();
+
+        GetUsernameFromTokenOutput getUsernameFromTokenOutput = GetUsernameFromTokenOutput.builder()
+                .username("domino222")
+                .build();
+
+
+        when(authenticationClient.loadUserDetails(any(LoadUserDetailsInput.class)))
+                .thenReturn(LoadUserDetailsOutput.builder().userDetails(userDetails).build());
+
+        ValidateAccessTokenOutput validateAccessTokenOutput = ValidateAccessTokenOutput.builder()
+                .success(true)
+                .build();
+
+        when(authenticationClient.getUsernameFromToken(any())).thenReturn(getUsernameFromTokenOutput);
+        when(authenticationClient.validateToken(any(ValidateAccessTokenInput.class))).thenReturn(validateAccessTokenOutput);
+
+        mockMvc.perform(post(RestAPIRoutes.BOOK_ROOM, roomId)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isForbidden());
+    }
 }
